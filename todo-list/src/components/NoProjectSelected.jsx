72d6cb092c870599e5179751bd622867c86b630f
Naioku <adrian.komuda.dev@gmail.com>
2025-02-12 20:10:14 +0100

    Added styling for NoProjectSelected.

--- todo-list/src/components/NoProjectSelected.jsx
+++ todo-list/src/components/NoProjectSelected.jsx
@@ -4,17 +4,28 @@ import {styled} from "styled-components";
 import Button from "./Button.jsx";
 import {H2} from "./Headers.jsx";
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 5rem;
+`
+
+const P = styled.p`
+    text-align: center;
+`
+
 const Img = styled.img`
-    width: 5rem;
+    width: 7rem;
 `
 
 export default function NoProjectSelected({onButtonClicked}) {
-    return <div>
+    return <Container>
         <Img src={noProjectImage} alt="no-projects"/>
         <H2>No Project Selected</H2>
-        <p>Select a project or get started with a new one.</p>
+        <P>Select a project or get started with a new one.</P>
         <Button onClick={onButtonClicked}>Create new project</Button>
-    </div>
+    </Container>
 }
 
 NoProjectSelected.propTypes = {
