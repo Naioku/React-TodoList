f43732a4874165bb6f8f8385918a6157f74d86bd
Naioku <adrian.komuda.dev@gmail.com>
2025-02-12 19:42:32 +0100

    Added styling for inputs.

--- todo-list/src/components/Input.jsx
+++ todo-list/src/components/Input.jsx
@@ -1,11 +1,33 @@
 ﻿import PropTypes from "prop-types";
 import {H3} from "./Headers.jsx";
+import {styled} from "styled-components";
+
+const Container = styled.div`
+    width: fit-content;
+`
+
+const InputInternal = styled.input`
+    height: 1.5rem;
+    width: 15rem;
+
+    &:focus {
+        border: 2px solid #646cff;
+        border-radius: 3px;
+        outline: none;
+        background-color: #3b3b3b;
+    }
+`
+
+const Label = styled(H3)`
+    margin: 0;
+    text-align: left;
+`
 
 export default function Input({label, ...props}) {
-    return <div>
-        <H3>{label}</H3>
-        <input {...props} />
-    </div>
+    return <Container>
+        <Label>{label}</Label>
+        <InputInternal {...props} />
+    </Container>
 }
 
 Input.propTypes = {
